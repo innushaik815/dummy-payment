30ec60972a9001f6bbf1246a9ad0241512db7609
SHAIK YOUNUS <39845907+innushaik815@users.noreply.github.com>
2024-02-15 15:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function submitPayment() {
+    // Simulate payment processing (in a real scenario, this would be replaced with actual payment processing code)
+    setTimeout(function() {
+        alert("Payment Completed");
+    }, 2000);
+
+    return false; // Prevent the form from actually submitting
+}
